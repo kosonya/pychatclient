d9f7ee0aae2431ef755470d9b4b54ead9c0cde84
maxikov <maxikov@thinkpad>
2013-03-02 13:10:45 +0400

    add nettools

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 import gtk
 import simpledialog
+import os
+import gobject
+import nettools
 
 class MessageArea(gtk.ScrolledWindow):
     def __init__(self, editable=False):
@@ -14,8 +17,13 @@ class MessageArea(gtk.ScrolledWindow):
 
         self.textview = gtk.TextView(self.textbuffer)
         self.textview.set_wrap_mode(gtk.WRAP_WORD)
-        self.textview.set_editable(False)
+        self.textview.set_editable(editable)
+        self.textview.connect('size-allocate', self.on_size_changed)
         self.add(self.textview)
+        
+    def on_size_changed(self, widget, event, data=None):
+        adj = self.get_vadjustment()
+        adj.set_value( adj.upper - adj.page_size )
         
     def set_text(self, text):
         self.textbuffer.set_text(text)
@@ -30,7 +38,7 @@ class MessageArea(gtk.ScrolledWindow):
 class MainWindow(gtk.Window):
     def __init__(self):
         gtk.Window.__init__(self)
-        self.set_default_size(300,300)
+        self.set_default_size(400,500)
         self.set_title(u"Наш крутой чат")
         self.mainbox = gtk.VBox()
         self.add(self.mainbox)
@@ -38,25 +46,67 @@ class MainWindow(gtk.Window):
         self.chat_area = MessageArea(editable = False)
         self.mainbox.pack_start(self.chat_area)
         
-        self.settings_button = gtk.Button(u"Настройки")
-        self.mainbox.pack_start(self.settings_button)
-        self.settings_button.connect("clicked", self.on_settings_button_clicked)
+        self.input_area = MessageArea(editable = True)
+        self.mainbox.pack_start(self.input_area, padding = 10)
         
+        self.buttons_box = gtk.HBox()
+        
+        self.settings_button = gtk.Button(u"Настройки")
+        self.buttons_box.pack_start(self.settings_button, padding = 10, fill = False)
+        self.settings_button.connect("clicked", self.on_settings_button_clicked)
+  
+        self.send_button = gtk.Button(u"Отправить")
+        self.buttons_box.pack_start(self.send_button, padding = 10, fill = False)
+
+        self.mainbox.pack_start(self.buttons_box, padding = 10, fill = False)
+
         self.connect("destroy", lambda _: gtk.main_quit())
         self.show_all()
+        
+        if os.path.exists("./settings.txt"):
+            f = open("settings.txt", "r")
+            lines = f.readlines()
+            f.close()
+            if len(lines) != 2:
+                settings_exist = False
+            else:
+                settings_exist = True
+        else:
+            settings_exist = False
+
+        
+        if not settings_exist:
+            self.chat_area.add_text(u"Нет сохранённых настроек. Надо бы настроить.")
+        else:
+            self.ip_port = lines[0][:-1]
+            self.nick = lines[1][:-1]
+            self.chat_area.add_text(u'Прочитали из настроек сервер: ' + self.ip_port)
+            self.chat_area.add_text(u'Прочитали из настроек ник: ' + self.nick)
+            self.connect_to_server()
+            
         
     def on_settings_button_clicked(self, widget):
         settings_dialog = simpledialog.SettingsDialog()
         response = settings_dialog.run()
         if response == gtk.RESPONSE_OK:
-            print settings_dialog.get_ip_port()
-            print settings_dialog.get_nick()
-            self.chat_area.add_text(settings_dialog.get_ip_port())
-            self.chat_area.add_text(settings_dialog.get_nick())
-
+            self.ip_port = settings_dialog.get_ip_port()
+            self.nick = settings_dialog.get_nick()
+            self.chat_area.add_text(u"Настроен сервер: " + self.ip_port)
+            self.chat_area.add_text(u"Настроен ник: " + self.nick)
+            f = open("settings.txt", "w")
+            f.write(self.ip_port + '\n')
+            f.write(self.nick + '\n')
+            f.close()
+            self.connect_to_server()
+            
         settings_dialog.destroy()
 
+    def connect_to_server(self):
+        connecter = nettools.Connecter(self.ip_port, self.chat_area)
+        connecter.start()
+
 def main():
+    gobject.threads_init()
     window = MainWindow()
     gtk.main()
 
---
+++ nettools.py
@@ -0,0 +1,35 @@
+# -coding: utf-8 -*-
+
+import threading
+import gobject
+import socket
+
+class Connecter(threading.Thread):
+    def __init__(self, ip_port, chat_area):
+        threading.Thread.__init__(self)
+        self.chat_area = chat_area
+        self.ip_port = ip_port
+            
+    def run(self):
+        #FIXME Не прекращает попытки содениения с сервером при измерении настроек.
+        try:
+            self.ip, self.port = self.ip_port.split(":")
+            self.port = int(self.port)
+            try:
+                gobject.idle_add(self.info_print, "Trying to connect to " + self.ip + ":"
+                             + str(self.port))
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect( (self.ip, self.port) )
+                gobject.idle_add(self.info_print, "Connected to " + self.ip + ":"
+                             + str(self.port))
+                s.close()    
+            except Exception as e:
+                gobject.idle_add(self.info_print, "Error: " + str(e))
+        except Exception as e:
+            gobject.idle_add(self.info_print, "Error: " + str(e))
+        
+        
+    def info_print(self, text):
+        self.chat_area.add_text(text)
+        return False
+    
